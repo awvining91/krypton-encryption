2b24892acf25f93cbce039375126948c06970331
Will Vining <awvining91@gmail.com>
2022-08-14 16:30:38 -0700

    added welcome alert and password length choice

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+confirm("Welcome to the password generator!")
+
+const aNumber = Number(window.prompt
+ ("Please pick a password length more than 8 and less than 12.", ""));
+
+   // then confirm("You have picked" aNumber, "!");
+
+console.log (aNumber);
+
+
+
+/*
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +25,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+*/
