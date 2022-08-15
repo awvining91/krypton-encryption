7fc8b591c4897fc1ad7016fa0687f0a754533ecb
Will Vining <awvining91@gmail.com>
2022-08-14 17:15:08 -0700

    added prompt for lowercase letters

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ confirm("Welcome to the password generator!")
 const aNumber = Number(window.prompt
  ("Please pick a password length more than 8 and less than 12.", ""));
 
+    console.log (aNumber);
+
+ const lowerCase = String(window.prompt
+    ("Do you want lowercase?", ""));
+
+    console.log (lowerCase);
+
    // then confirm("You have picked" aNumber, "!");
 
  //This is here for debugging purposes to see what passes through  
 
-console.log (aNumber);
+
 
 //Do you want lowercase letters?
     /*var lowerCase = (window.prompt
@@ -28,6 +35,16 @@ console.log (aNumber);
 
 console.log (lowerCase);
 */
+
+/*var lowerCase = prompt("Do you want lowercase letters?");
+
+if (lowerCase != null) {
+    prompt("You have chosen" + lowerCase "!")
+
+
+}
+*?
+
 
 /*
 // Assignment Code
